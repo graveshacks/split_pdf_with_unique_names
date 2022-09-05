fa296ed26cb27d7dbdb83608504f405ce48df5fe
Shantanu Salimath <57179857+graveshacks@users.noreply.github.com>
2022-09-05 11:02:04 +0530

    Checks special characters in file name
    Introduced RegEx to check for special characters in file names in text file as most OS allow only certain characters and deject others. The characters listed in this code are not allowed in windows only.

--- pdf_split.py
+++ pdf_split.py
@@ -1,5 +1,5 @@
-
 from PyPDF2 import PdfFileReader, PdfFileWriter
+import re
 
 pdf_file_path = 'Back_packs_BCP_1372.pdf'   # enter name of the PDF file to be extracted from
 file_base_name = pdf_file_path.replace('Back_packs_BCP_1372.pdf', '')
@@ -9,7 +9,7 @@ pdf = PdfFileReader(pdf_file_path)
 totalpages = pdf.numPages
 print("Total number of pages in the pdf: ", totalpages)
 pgs = []
-with open("page_range.txt", "r") as f:
+with open("page_range.txt", "r") as f:   # enter text file containing page range and names
     lines = f.readlines()
 for line in lines:
     pg = line.split("\t")
@@ -21,13 +21,14 @@ for x in pgs:
         for y in pg_range:
             if len(pg_range)==2:
                 m = int(pg_range[0])
-                n = int(pg_range[1])
+                n = int(pg_range[1])  # if page range is multiple page
             else:
                 m = int(pg_range[0])
-                n = m
+                n = m                 # if single page 
             string = x[1]
-            new_name = string.strip('\n')
-            pages = []
+            name = string.strip('\n')
+            new_name = re.sub("[/\?*:|<>]", "_", name)  # to recognize and replace characters not supported in files by windows
+            pages = []                                  # supported characters depends on the respective OS
             for p in range(m,n+1):
                 pages.append(p-1)
 
