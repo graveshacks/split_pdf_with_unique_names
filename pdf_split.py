f22c3ed106d8aa50ee6a20c2890e1b959d4879ab
Shantanu Salimath <57179857+graveshacks@users.noreply.github.com>
2022-09-08 12:10:17 +0530

    Code structured within function
    Structured the code within a function to take TWO user inputs,  viz. Source PDF file name/Path name which is to be split and the text file name/Path name containing page range and names.

--- pdf_split.py
+++ pdf_split.py
@@ -1,41 +1,46 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import re
 
-pdf_file_path = 'Back_packs_BCP_1372.pdf'   # enter name of the PDF file to be extracted from
-file_base_name = pdf_file_path.replace('Back_packs_BCP_1372.pdf', '')
+def pdf_split_mult(pdf_file_path, text_file):
+    file_base_name = pdf_file_path.replace(pdf_file_path, '')
 
-pdf = PdfFileReader(pdf_file_path)
+    pdf = PdfFileReader(pdf_file_path)
 
-totalpages = pdf.numPages
-print("Total number of pages in the pdf: ", totalpages)
-pgs = []
-with open("page_range.txt", "r") as f:   # enter text file containing page range and names
-    lines = f.readlines()
-for line in lines:
-    pg = line.split("\t")
-    pgs.append(pg)
-for x in pgs:
-    for l in x:
-        e = x[0]
-        pg_range = e.split("-")
-        for y in pg_range:
-            if len(pg_range)==2:
-                m = int(pg_range[0])
-                n = int(pg_range[1])  # if page range is multiple page
-            else:
-                m = int(pg_range[0])
-                n = m                 # if single page 
-            string = x[1]
-            name = string.strip('\n')
-            new_name = re.sub("[/\?*:|<>]", "_", name)  # to recognize and replace characters not supported in files by windows
-            pages = []                                  # supported characters depends on the respective OS
-            for p in range(m,n+1):
-                pages.append(p-1)
+    totalpages = pdf.numPages
+    print("Total number of pages in the pdf: ", totalpages) # prints total number of pages in the source PDF
+    pgs = []
+    with open(text_file, "r") as f:   # enter text file containing page range and names
+        lines = f.readlines()
+    for line in lines:
+        pg = line.split("\t")  # splits page range and respective names into list
+        pgs.append(pg)
+    for x in pgs:
+        for l in x:
+            e = x[0]
+            pg_range = e.split("-")  # splits page range into individual pages into a list
+            for y in pg_range:
+                if len(pg_range)==2:
+                    m = int(pg_range[0])
+                    n = int(pg_range[1])  # if page range is multiple page
+                else:
+                    m = int(pg_range[0])
+                    n = m                 # if single page 
+                string = x[1]
+                name = string.strip('\n')
+                new_name = re.sub("[/\?*:|<>]", "_", name)  # to recognize and replace characters not supported in files by windows
+                pages = []                                  # supported characters depends on the respective OS
+                for p in range(m,n+1):
+                    pages.append(p-1)
 
-            pdfWriter = PdfFileWriter()
-            for page_num in pages:
-                pdfWriter.addPage(pdf.getPage(page_num))
+                pdfWriter = PdfFileWriter()
+                for page_num in pages:
+                    pdfWriter.addPage(pdf.getPage(page_num))
 
-            with open(f'{new_name}.pdf', 'wb') as f:
-                pdfWriter.write(f)
-                f.close()
+                with open(f'{new_name}.pdf', 'wb') as f:
+                    pdfWriter.write(f)
+                    f.close()
+
+if __name__ == '__main__':
+    a = input("Enter source PDF file name/path: ")
+    b = input("Enter text file name/path: ")
+    pdf_split_mult(a,b)
